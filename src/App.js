f9243e89906d5c2f07937f5d10a0826e16a25e54
omaralezzi <81753019+omaralezzi@users.noreply.github.com>
2021-11-09 18:55:13 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// You don't need to change this file!
+// Take a look at 'validator.js' instead!
+
+import React from "react";
+import Validator from "./Validator";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Validator />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+        />
+      </div>
+    );
+  }
+}
